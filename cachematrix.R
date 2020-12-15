e0e37bdbc1aa540c67a5b9e72ec92b78afb1b5a6
vik-hegde <vikas1.hegde@gmail.com>
2020-12-15 22:33:05 +0530

    assignment2-Lexical-scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## Function to store and retrieve inverted matrix 
+makeCacheMatrix <- function(x = matrix()) 
+{
+#Initialize a null matrix
+  m <- matrix(list(NULL),nrow(x),ncol(x))
+#
+  set <- function(y) 
+  {
+    x <<- y
+    m <<- matrix()
+  }
+  get <- function() x
+#  
+#Stores the inverted matrix from cachesolve function
+  setinv <- function(inv) m <<- inv 
+  getinv <- function() m
+  list(set = set, get = get,setinv = setinv,getinv = getinv)
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#
+## Function to calculate inverted matrix
+cacheSolve <- function(x, ...) 
+{
+  count=0
+  elm_m=c()
+  m <- x$getinv()
+# loop to verify if each element of m is NULL or not
+  elm_m<-sapply(m,is.null)
+  for(i in elm_m)
+  {
+    if(isTRUE(i)) 
+    {
+      count<-count+1
+    }
+  }
+#if m is not NULL cached m will be retrieved
+  if (count!=nrow(m)^2)
+  {
+    message("getting cached data")
+    return(m)
+  }
+#if m is NULL new inverse will be calculated
+  data <- x$get()
+  m <- solve(data)
+  x$setinv(m)
+  m
 }
